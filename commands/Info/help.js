f7f1b7b80dcfa6b48006a67faae6956774ded1a0
kirigizakazutovn <83147867+kirigizakazutovn@users.noreply.github.com>
2021-12-24 15:46:10 +0700

    vn

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -13,7 +13,7 @@ module.exports = {
   aliases: ["h", "halp", "helpme", "hilfe"],
 
   cooldown: 1, //the command cooldown for execution & for helpcmd [OPTIONAL]
-  description: "Returns all Commmands, or one specific command", //the command description for helpcmd [OPTIONAL]
+  description: "Trả lại tất cả các lệnh/một lệnh nào đó", //the command description for helpcmd [OPTIONAL]
   memberpermissions: [], //Only allow members with specific Permissions to execute a Commmand [OPTIONAL]
   requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
   alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
