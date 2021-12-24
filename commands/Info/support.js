2256fa1ef92594266c7d6f950bdd01334df32349
kirigizakazutovn <83147867+kirigizakazutovn@users.noreply.github.com>
2021-12-24 16:00:09 +0700

    Việt hóa Unicode

--- commands/Info/support.js
+++ commands/Info/support.js
@@ -12,7 +12,7 @@ module.exports = {
   aliases: ["server"],
 
   cooldown: 1, //the command cooldown for execution & for helpcmd [OPTIONAL]
-  description: "Sends a Link of the Support Server", //the command description for helpcmd [OPTIONAL]
+  description: "Gửi đường link đến support sever", //the command description for helpcmd [OPTIONAL]
   memberpermissions: [], //Only allow members with specific Permissions to execute a Commmand [OPTIONAL]
   requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
   alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
