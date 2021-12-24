94870fa76ee018b94e75b7207bb2cda6f7afafa1
kirigizakazutovn <83147867+kirigizakazutovn@users.noreply.github.com>
2021-12-24 16:02:05 +0700

    Update bot bằng Unicode

--- commands/Info/commandcount.js
+++ commands/Info/commandcount.js
@@ -10,7 +10,7 @@ module.exports = {
   usage: "commandcount",
   aliases: ["cmds", "commandc", "count", "cmdcount"],
   cooldown: 1, //the command cooldown for execution & for helpcmd [OPTIONAL]
-  description: "Shows the Amount of Commands an Categories", //the command description for helpcmd [OPTIONAL]
+  description: "Hiển thị số lượng lệnh trong 1 danh mục", //the command description for helpcmd [OPTIONAL]
   memberpermissions: [], //Only allow members with specific Permissions to execute a Commmand [OPTIONAL]
   requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
   alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
